edac845b0ac1b9da0a4017b44d58e9af332aecc9
Phuc Pham N <michaelphamn@yahoo.com>
2020-07-20 21:45:56 -0700

    Commit files

---
+++ Phuc Pham N - Exercise 3/js/app.js
@@ -0,0 +1,18 @@
+$(document).read(function(){
+    // $(".nav li").each(function(index)
+    // {
+    //     $(this).click(function()  {                   
+    //         $(this).addClass("gray");  
+    //         html = "<div>";
+    //         html = html.concat("<img src='images/medium/" + people[index].path + "'");                    
+    //         html = html.concat(" alt='" + people[index].title + "'/>");
+    //         html = html.concat("<p>" + people[index].title + "</p>");
+    //         html = html.concat("<p>" + people[index].city + ", " + people[index].country + " [" + people[index].taken + "]" + "</p>");
+    //         html = html.concat("</div>");
+    //     });
+    //     $(this).mouseleave(function()  {       
+    //         $(this).removeClass("gray");
+    //         $("#preview").hide();            
+    //     });   
+    // });
+});
